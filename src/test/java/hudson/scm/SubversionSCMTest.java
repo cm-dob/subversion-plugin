05d4a743ceab3026599e6a448b0969d955e79911
Alexander <alexander.ukolov@dev.zodiac.tv>
2014-09-06 18:08:00 +0400

    Fixed compilation error
    Sort moduleLocations by localDir
    
    Add workspace updater class for reverting externals
    
    Modified git tag

--- src/test/java/hudson/scm/SubversionSCMTest.java
+++ src/test/java/hudson/scm/SubversionSCMTest.java
@@ -697,8 +697,7 @@ public class SubversionSCMTest extends AbstractSubversionTest {
         String[] invalidUsernames = new String[] {
             "\\user",
             "DOMAIN\\",
-            "DOMAIN@user",
-            "DOMAIN.user" };
+            "DOMAIN@user" };
 
         for (String invalidUsername : invalidUsernames) {
             assertEquals(
